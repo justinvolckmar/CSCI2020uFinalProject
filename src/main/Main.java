c0ac660dc78a60f2e426d702c5e3210215d53130
Justin Volckmar <justin.volckmar@uoit.net>
2020-03-02 17:43:53 -0500

    Default window application built

---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) { launch(args); }
+
+	public void start(Stage window) throws Exception {
+		Pane root = (Pane) FXMLLoader.load(getClass().getResource("Main.fxml"));
+		Scene scene = new Scene(root, 600, 400);
+		scene.getStylesheets().add(getClass().getResource("Main.css").toExternalForm());
+		window.setScene(scene);
+		window.setTitle("Final Project Application Window");
+		window.show();
+	}
+
+}

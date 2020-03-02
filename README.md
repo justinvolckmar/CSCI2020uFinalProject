860da7f1999a91750fad08cae94b72362a7e5ebc
Justin Volckmar <justin.volckmar@uoit.net>
2020-03-02 17:49:19 -0500

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CSCI2020uFinalProject
 CSCI2020u (Software Systems) Final Project
+-Default files that can be renamed based on their decided purpose.
--- src/main/Main.java
+++ src/main/Main.java
@@ -11,9 +11,10 @@ public class Main extends Application {
 	public static void main(String[] args) { launch(args); }
 
 	public void start(Stage window) throws Exception {
-		Pane root = (Pane) FXMLLoader.load(getClass().getResource("Main.fxml"));
+		Pane root = new Pane();
+		//Pane root = (Pane) FXMLLoader.load(getClass().getResource("../main/Main.fxml"));
 		Scene scene = new Scene(root, 600, 400);
-		scene.getStylesheets().add(getClass().getResource("Main.css").toExternalForm());
+		scene.getStylesheets().add(getClass().getResource("../main/Main.css").toExternalForm());
 		window.setScene(scene);
 		window.setTitle("Final Project Application Window");
 		window.show();

cc65bf2f3e47582825986913899ba985c867c83c
Justin Volckmar <justin.volckmar@uoit.net>
2020-03-11 17:37:16 -0400

    Added TODOs and setup to start implementation of socket io

--- src/main/Minesweeper.java
+++ src/main/Minesweeper.java
@@ -1,5 +1,10 @@
 package main;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -17,64 +22,78 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Minesweeper {
+
+	//TODO add timer on separate thread
+	//TODO make an update listener for the timer to update the timer field to the user
 	
+	//TODO make a Server.java which takes in the data from the end of the game and stores 
+	//a list of all scores, only returning the top 10 scores and the users current score/place in topscores
+	//(so 11 scores max displayed)
+	//TODO use a TableView to display the data returned from server and read and write data entries to csv file stored in server
+	//TODO also use threading within the server/client send/recieve scopes
+	
+	protected Socket socket;
+	protected DataInputStream fromServer;
+	protected DataOutputStream toServer;
+
 	//nested layout placeholders
 	protected Stage window;
 	protected VBox root;
 	protected HBox optionsBox, layout;
-	
+
 	//grid for buttons
 	protected GridPane buttonBox;
 	protected GridSlot[][] grid;
-	
+
 	//toggle for flags
 	protected ToggleGroup group;
 	protected RadioButton flag, select;
-	
+
 	//ui control
 	protected Label gameOver, title;
 	protected TextField flags, mine, score, moves;
-	
+
 	//size is (nxn) of grid, total # mines, buttonSize (based on buttons#), total score counter, total flag counter, and move tracker
 	protected int size, mines, buttonSize, totalScore, totalFlags, numMoves;
-	
+
 	public class GridSlot extends Button {
 		protected boolean hasMine, hasFlag;
 		protected int x, y, score;
 		public GridSlot(int x, int y) { hasMine = false; hasFlag = false; this.x = x; this.y = y; this.score = 0; }
 	}
-	
+
 	public Minesweeper(Stage window, int size, int mines) throws Exception {
 		//store default values
 		this.window = window;
 		this.size = size;
 		this.mines = mines;
-		//initialize root for minesweeper board
-		start();
-	}
-	
-	public void start() throws Exception {
+
+		//init the server port and streams
+		socket = new Socket("localhost", 8000);
+		fromServer = new DataInputStream(socket.getInputStream());
+		toServer = new DataOutputStream(socket.getOutputStream());
+
 		//create root box
 		root = new VBox();
 		root.setSpacing(20);
 		root.setPadding(new Insets(20,20,20,20));
 		root.setAlignment(Pos.CENTER);
-		
+
 		//set the scene to root
 		Scene scene = new Scene(root,1920,1080);
 		scene.getStylesheets().add(getClass().getResource("../main/Main.css").toExternalForm());
-		
+
 		//create side box for data
 		layout = new HBox();
 		layout.setSpacing(20);
 		layout.setPadding(new Insets(20,20,20,20));
 		layout.setAlignment(Pos.CENTER);
-		
+
 		//create options box for toggles
 		optionsBox = new HBox();
 		optionsBox.setSpacing(10);
 		optionsBox.setAlignment(Pos.CENTER);
-		
+
 		//create toggle group and toggles
 		group = new ToggleGroup();
 		flag = new RadioButton("Flag");
@@ -83,11 +102,11 @@ public class Minesweeper {
 		select.setToggleGroup(group);
 		select.setSelected(true);
 		optionsBox.getChildren().addAll(select, flag);
-		
+
 		//initialize labels
 		title = new Label("Minesweeper (" + size + "x" + size +")");
 		gameOver = new Label();
-		
+
 		//create gridpane for GridSlot buttons
 		buttonBox = new GridPane();
 		buttonBox.setAlignment(Pos.CENTER);
@@ -134,8 +153,8 @@ public class Minesweeper {
 		window.setMaximized(true);
 		//window.setFullScreen(true);
 		window.show();
-	}//end of start()
-	
+	}
+
 	private void setMines(GridSlot slot) {
 		int mineCount = 0;
 		while (mineCount < mines) { //randomly place x amount of mines until they have all been placed
@@ -156,32 +175,32 @@ public class Minesweeper {
 		VBox data = new VBox();
 		data.setSpacing(20);
 		data.setAlignment(Pos.CENTER);
-		
+
 		//create score output
 		score = new TextField("Score: " + totalScore);
 		score.setEditable(false);
 		score.setFocusTraversable(false);
-		
+
 		//create num moves output
 		moves = new TextField("Moves: " + numMoves);
 		moves.setEditable(false);
 		moves.setFocusTraversable(false);
-		
+
 		//create num flags output
 		flags = new TextField("Total Flags: " + totalFlags);
 		flags.setEditable(false);
 		flags.setFocusTraversable(false);
-		
+
 		//create num mines output
 		mine = new TextField("Total Mines: " + mines);
 		mine.setEditable(false);
 		mine.setFocusTraversable(false);
-		
+
 		//add all outputs to data and return
 		data.getChildren().addAll(score, moves, flags, mine);
 		return data;
 	}
-	
+
 	private void findScores() {
 		//loop for all values in the grid
 		for (int i = 0 ; i < size ; i++) {
@@ -209,10 +228,28 @@ public class Minesweeper {
 			gameOver.setText("You flagged all the mines! You win. Restart?");
 			gameOver.setTextFill(Color.GREEN);
 		}
+		//button to begin new game
 		Button restart = new Button("New Game");
 		restart.getStyleClass().add("fxbutton");
 		restart.setOnAction(e -> { window.setScene(Main.scene); }); //start a new game
-		optionsBox.getChildren().addAll(gameOver, restart);//add a new game button and win/loss message
+		//button to view top scores
+		Button topScores = new Button("View Top Scores");
+		topScores.getStyleClass().add("fxbutton");
+		topScores.setOnAction(e -> {
+			try {
+				toServer.writeInt(totalScore);
+				toServer.writeInt(totalFlags);
+				toServer.writeInt(size);
+				toServer.writeInt(mines);
+				toServer.writeInt(numMoves);
+				
+				for (int i = 0 ; i < 10 ; i++) {
+					//fromServer - TODO potentially read in 10x times of each "line" in top 10 with each point being an int possibly
+					//TODO create display method for top 10 score values
+				}
+			} catch (IOException e1) { e1.printStackTrace(); }
+		});
+		optionsBox.getChildren().addAll(gameOver, restart, topScores);//add a new game button and win/loss message
 		for (int i = 0 ; i < size ; i++) {
 			for (int j = 0 ; j < size ; j++) {
 				if (!grid[i][j].isDisabled()) {//if a grid slot is not disabled, disable it and check whether or not it's score
@@ -238,7 +275,7 @@ public class Minesweeper {
 			}
 		}
 	}
-	
+
 	private boolean checkMines() {//returns true if any mine is still unflagged at end of game
 		boolean mineUnflagged = false;//returns false if all mines are flagged at end of game
 		for (int i = 0 ; i < size ; i++) {
@@ -251,7 +288,7 @@ public class Minesweeper {
 		return mineUnflagged;
 	}
 
-	
+
 	public void onAction(GridSlot slot) {
 		if (slot.hasMine || !checkMines()) {
 			gameOver();

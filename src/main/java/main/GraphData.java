120db97a92dea34637334651abe740ac62644019
John Hruda <55451976+TreyBombCity@users.noreply.github.com>
2020-03-22 17:16:31 -0400

    uploaded data display graph

---
+++ src/main/java/main/GraphData.java
@@ -0,0 +1,78 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.chart.*;
+import javafx.stage.Stage;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class GraphData extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Graph Title");
+
+        //defining the axes
+        final CategoryAxis xAxis = new CategoryAxis(); // we are gonna plot against time
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Time/s");
+        xAxis.setAnimated(false); // axis animations are removed
+        yAxis.setLabel("Value");
+        yAxis.setAnimated(false); // axis animations are removed
+
+        //creating the line chart with two axis created above
+        final LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Data Graph");
+        lineChart.setAnimated(false); // disable animations
+
+        //defining a series to display data
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Data Series");
+
+        // add series to chart
+        lineChart.getData().add(series);
+
+        // this is used to display time in HH:mm:ss format
+        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+
+        // setup a scheduled executor to periodically put data into the chart
+        ScheduledExecutorService scheduledExecutorService;
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+
+        // put data onto graph per second
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+
+
+            // get a random integer between 0-10 CHANGE THIS LINE TO PULL DATA FROM GAME
+            //IF YOU NEED TO CONVERT AN INT TO INTEGER JUST USE:
+            //int i = 5;
+            //Integer i2 = new Integer(i);
+            //THIS IS BECAUSE THE SERIES DATA NEEDS TO TAKE AN INTEGER
+            Integer random = ThreadLocalRandom.current().nextInt(10);
+
+
+            // Update the chart
+            Platform.runLater(() -> {
+                // get current time
+                Date now = new Date();
+                // put random number with current time
+                series.getData().add(new XYChart.Data<>(simpleDateFormat.format(now), random));
+            });
+        }, 0, 1, TimeUnit.SECONDS);
+
+        // setup scene
+        Scene scene = new Scene(lineChart, 800, 600);
+        primaryStage.setScene(scene);
+
+        // show the stage
+        primaryStage.show();
+    }
+}
